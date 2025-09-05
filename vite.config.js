5d06bbc1774c11e0080e5abb5bd4f49612d5f352
aritra sharma <98529719+aritra1189@users.noreply.github.com>
2025-09-05 15:39:27 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss()
-  ],
+  plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://192.168.29.242:6524", // your backend
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
